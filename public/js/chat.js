05efc81ebb7db00d5b1c73dc6eafba8a6c3a6526
Bohdan Melvic <bohdanmelvic@gmail.com>
2019-11-11 12:54:40 +0200

    added messages logic

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,29 @@
-io();
+const socket = io();
+const messageForm = document.querySelector('#messageForm');
+const sendLocationBtn = document.querySelector('#sendLocationBtn');
+
+socket.on('message', (message) => {
+    console.log(message);
+});
+
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = document.querySelector('.inputText').value;
+
+    socket.emit('sendMessage', message, (textFormServer) => {
+        console.log('Message delivired.', textFormServer);
+    });
+});
+
+sendLocationBtn.addEventListener('click', (e) => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.')
+    }
+
+    navigator.geolocation.getCurrentPosition( (position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude:  position.coords.longitude
+        });
+    });
+});
--- src/index.js
+++ src/index.js
@@ -12,9 +12,30 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => {
+// socket.emit - send data only one user which connected by 'someChannelName'
+// io.emit - send data to ALL users which connected by 'someChannelName'
+// socket.broadcast - send data to all users except current user whichs connected by 'someChannelName'
+// socket.on - received data from user which connected by 'someChannelName'
+
+io.on('connection', (socket) => {
     console.log('New WebSocket connection');
-});
+    
+    socket.emit('message', 'Welcome!');
+    socket.broadcast.emit('message', "A new user has joined"); 
+
+    socket.on('sendMessage', (message, callback) => { 
+        io.emit('message', message);
+        callback('Delivired!');
+    });
+
+    socket.on('sendLocation', (location) => { 
+        socket.broadcast.emit('message', `https://google.com/maps?q=${location.latitude},${location.longitude}`);
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'User has leaved a chat!');
+    });
+}); 
 
 server.listen(port, () => {
     console.log(`Server is up on port ${port}!`);
