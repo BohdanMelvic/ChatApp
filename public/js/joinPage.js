0a3ab4aa8ba14063b95dbf9265ea6d5108dd4ccd
Bohdan Melvic <bohdanmelvic@gmail.com>
2019-11-25 01:01:06 +0200

    added avatars chose

--- public/js/joinPage.js
+++ public/js/joinPage.js
@@ -13,3 +13,14 @@ socket.on('roomsList', ({ rooms }) => {
     console.log(rooms);
 });
 
+// Avatar 
+
+const selectAvatar = function(element) {
+    const avatarPicture = jQuery(element);
+    const allAvatars = jQuery(".avatarSelection");
+    for (let i = 0; i < allAvatars.length; i += 1) {
+      jQuery(allAvatars[i]).removeClass("avatar-active");
+    }
+    avatarPicture.addClass("avatar-active");
+    avatarPicture.find('input').prop("checked", true);
+  };
